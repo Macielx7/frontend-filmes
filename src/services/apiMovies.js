7583c2e065f04b8a19accd444da3a8cfa4d70ed3
Macielx7 <davimacielbsb@gmail.com>
2024-09-08 21:02:38 -0300

    apiMovies

---
+++ src/services/apiMovies.js
@@ -0,0 +1,14 @@
+const { default: axios } = require("axios");
+
+
+const apiMovie = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    params:{
+        language: 'pt-BR'
+    },
+    headers: {
+        Authorization: 'Bearer 
+    }
+})
+
+export default apiMovie  
